4bdc1152ae79a73d99e73f9a0f46d7be4064cd37
Paul Kasidit <paulkasidit@gmail.com>
2022-09-14 14:55:03 -0700

    create anagram script file

--- lib/antigrams_or_anagrams.rb
+++ lib/antigrams_or_anagrams.rb
@@ -73,16 +73,3 @@ class Analyzer
 
 end
 
-puts "\n"
-puts "Hey there, please enter two words for antigram/anagram analysis on the prompt"
-puts "\n"
-puts "\n"
-puts "Enter first word/sentence" 
-first_string = gets.chomp
-puts "\n"
-puts "Enter second word/sentence" 
-second_string = gets.chomp
-puts "\n"
-
-first_analysis = Analyzer.new(first_string,second_string) 
-first_analysis.anagram_or_antigram() 
